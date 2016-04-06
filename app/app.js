a8a66fe04d224a0fcd452a900e62a139ba3db90e
Robson da Silva <sc.robson@gmail.com>
2016-04-06 13:05:35 -0300

    Adding angular-ui-router

--- app/app.js
+++ app/app.js
@@ -1,6 +1,6 @@
 (function () {
     'use strict';
 
-    angular.module('rsilva.net.app', ['pascalprecht.translate']);
+    angular.module('rsilva.net.app', ['ui.router', 'pascalprecht.translate']);
     
 })();
